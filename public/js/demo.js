439e44493dd49e6f47dc35ebf923e8d25cef1827
okada9876 <Thyago.Matos@voith.com>
2017-01-26 19:30:01 -0200

    teste

--- public/js/demo.js
+++ public/js/demo.js
@@ -201,7 +201,7 @@ $(document).ready(function() {
   function showTextSummary(data) {
     console.log('showTextSummary()');
     var paragraphs = textSummary.assemble(data.tree);
-    var div = $('.summary-div');
+    var div = $('.summary-div'+' t');
     $('.outputMessageFootnote').text(data.word_count_message ? '**' + data.word_count_message + '.' : '');
     div.empty();
     paragraphs.forEach(function(sentences) {
