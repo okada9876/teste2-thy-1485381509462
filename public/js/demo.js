a4386492b6ed9e474f5bec21c24a10b46927ebbc
okada9876 <Thyago.Matos@voith.com>
2017-01-26 22:32:07 -0200

    oidsa

--- public/js/demo.js
+++ public/js/demo.js
@@ -296,8 +296,8 @@ function showVizualization(theProfile) {
         height = this.dimH;
 
       // The flower had a radius of 640 / 1.9 = 336.84 in the original, now is 3.2.
-      var radius = Math.min(width, height) / 16.58; // For 640 / 1.9 -> r = 65
-      var scaled_w = radius * 1.0; // r = 65 -> w = 160
+      var radius = Math.min(width, height) / 10.0; // For 640 / 1.9 -> r = 65
+      var scaled_w = radius * 3.0; // r = 65 -> w = 160
 
       var id = 'user_icon_' + this.id;
       icon_defs.append('pattern')
